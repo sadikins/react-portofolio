981ba4abce36a31c62597dba6541ab043d95a4ba
sadikins <msadikin1989@gmail.com>
2022-06-15 09:04:45 +0700

    first commit

---
+++ src/container/About/About.jsx
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react'
+import {motion} from 'framer-motion'
+
+import './About.scss'
+
+const abouts = [
+  {title: 'Web Development', description: 'I am a good web developer', imgUrl:''},
+  {title: 'Web Design', description: 'I am a good web developer', imgUrl:''},
+  {title: 'UI/UX', description: 'I am a good web developer', imgUrl:''},
+  {title: 'Web Animation', description: 'I am a good web developer', imgUrl:''},
+]
+
+const About = () => {
+
+  // useEffect(() => {
+  //   const query = '*[_type == "abouts"]';
+
+  //   client.fetch(query).then((data) => {
+  //     setAbouts(data)
+  //   })
+  // },[])
+
+
+  return (
+    <>
+    
+    <h2 className='head-text'>
+      I Know that 
+      <span>Good Design</span> <br /> 
+      means <span>Good Business</span></h2>
+
+    <div className="app__profiles">
+      {abouts.map((about, index) => (
+        <motion.div
+        whileInView={{ opacity:1 }}
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.5, type: 'tween' }}
+        className="app__profile-item"
+        key={about.title + index}
+        
+        
+        >
+          <img src={about.imgUrl} alt={about.title} />
+          <h2 className="bold-text" style={{ marginTop:20 }}>
+           {about.title}
+          </h2>
+          <p className='p-text' style={{ marginTop:10 }}>
+            {about.description}
+          </p>
+        </motion.div>
+      ))}
+    </div>
+    </>
+  )
+}
+
+export default About
